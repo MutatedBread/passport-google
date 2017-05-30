10b68cb78ebc4ac38f11ad62e9971fd6196c7440
MutatedBread <chenhon199@yahoo.com>
2017-05-30 23:30:42 +0800

    add stuff to run on heroku

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,8 @@
 var authConfig = {
     'googleAuth': {
-        'clientId': '248739145829-jlot6r907j5kogqq1b0rljkbs5va7iai.apps.googleusercontent.com',
-        'clientSecret': '7zdByp5pWouJCzESwHlk1GO4',
-        'callBack': 'http://localhost:3000/auth/google/callback'
+        'clientId': '[CLIENT_ID]]',
+        'clientSecret': '[CLIENT_SECRET]',
+        'callBack': '[CALL_BACK_URL]'
     }
 };
 
